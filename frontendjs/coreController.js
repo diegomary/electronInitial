cc290950a005adec07685aa2013b2b0df58fea7d
diegomary <diegoaldoburlando62@gmail.com>
2018-12-18 11:37:22 +0100

    First push

--- frontendjs/coreController.js
+++ frontendjs/coreController.js
@@ -7,6 +7,24 @@
     .controller("CoreCtrl", ["Title", CoreCtrl]);
     function CoreCtrl(Title) {      
       this.title = Title.value;      
-      angular.element(document).ready( () => {});     
+      angular.element(document).ready( () => {
+
+
+        var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/javascript");
+        var renderer = require('electron').ipcRenderer;
+        renderer.on('vdone', (event, message) => {
+          editor.session.setMode("ace/mode/text");           
+          editor.setValue(message);
+          editor.clearSelection(); 
+        })
+        renderer.on('htmlchecker', (event, message) => {
+          editor.session.setMode("ace/mode/html");           
+          editor.setValue(message);
+          editor.clearSelection(); 
+        })
+
+      });     
     }
   })();
--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const {app, BrowserWindow,Menu} = require('electron');
+const {app, BrowserWindow, Menu, dialog,ipcMain } = require('electron');
 const path = require('path');
 const url = require('url'); 
 const shell = require('electron').shell;
-  
+const writeFile = require('write');
+const express = require('express');
+var appexpress = express();
+
+  appexpress.get('/',  (req, res) => {
+    res.send('Hello World')
+  }); 
+  appexpress.listen(3000);
+  console.log("Server express listening on http://localhost:3000");
+    
   let win;  
   function createWindow () {
    
@@ -13,6 +22,62 @@ const shell = require('electron').shell;
 
     var menu = Menu.buildFromTemplate([
       { label :"Tools", submenu : [
+          // File Management
+          { label:"Create Menu Action", click() {
+
+            dialog.showOpenDialog (
+             {  title:"Open File to elaborate",
+                securityScopedBookmarks :true,
+                properties: ['openFile', 'createDirectory'],
+                filters: [                
+                {name: 'Javascript Notation Type', extensions: ['json']},
+                {name: 'All Files', extensions: ['*']}
+              ]
+             }, (filePath,bookmarks ) => {
+
+                if(filePath === undefined) return;
+                // Here we can elaborate our file
+               console.log("Logic here");
+               
+                dialog.showSaveDialog( {title:"Save file Vocab",filters:[{name: "Vocab Bixby file", extensions:['vocab.6t']}]},
+                (filename) => {
+                  if(filename === undefined) return;
+                  writeFile.sync(filename, total);                                
+                  //win.loadFile('vocabdone.html');
+                  win.webContents.send('vdone',total);
+                });
+               }
+            );           
+     
+          }},
+
+          // Addtrainings
+          { label:"TODO",click() {
+            dialog.showOpenDialog (
+              {  title:"Choose File",                
+                 properties: ['openFile'],
+                 filters: [                
+                 {name: 'Executable files', extensions: ['exe']}
+               ]
+              }, (filePath) => {
+                console.log(filePath[0]);
+              });              
+          }},
+
+          // Html Code checker
+          { label:"Html code checker",click() {
+            win.webContents.send('htmlchecker',`<!DOCTYPE html>
+            <html>
+            <head>            
+            </head>
+            <body>
+              <h1>Html checker</h1>
+              <p>Paste html in place of this to check tags closure</p>
+            </body>
+            </html>
+            `);
+          }},
+
           // DevTools
           { label:"Open Dev Tools",click() {
           win.webContents.openDevTools();
@@ -22,8 +87,8 @@ const shell = require('electron').shell;
             app.quit();
           }},
           { type: 'separator'},
-          { label:"Diego Website",click() {
-            shell.openExternal('https://diegomary.github.io/#/about');
+          { label:"DM88's Website",click() {
+            shell.openExternal('https://diegomary.github.io');
           }},
           // More ....
         ]    
